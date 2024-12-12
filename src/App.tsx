5c952fdceed8a1307fb6a3bb0e77eb946ac3b887
SylvHardDev <hardsyldev@gmail.com>
2024-12-12 12:19:33 +0300

    import login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <>
       <Login/>
+      {/* <Register/> */}
     </>
   );
 }
