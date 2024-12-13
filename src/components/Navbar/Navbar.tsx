d8833fdbb7ca455e4ac0ee13276bfa60c4afd81f
SylvHardDev <hardsyldev@gmail.com>
2024-12-13 10:07:59 +0300

    background linear

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,9 @@
 import { Search, Bell, HelpCircle } from "lucide-react";
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between px-6 py-4 bg-gradient-to-b from-gray-50 to-gray-100 border-b">
+    <div className="navbar flex items-center justify-between px-6 py-4 bg-gradient-to-b from-gray-50 to-gray-100 border-b">
       {/* Left Section */}
       <div className="flex items-center">
         <div>
@@ -13,15 +14,15 @@ const Navbar = () => {
       </div>
 
       {/* Center Section: Search Bar */}
-      <div className="flex-1 mx-6">
-        <div className="relative w-full">
+      <div className="flex-1 mx-6 w-1/2 h-full flex items-center justify-center ">
+        <div className="relative shadow-sm bg-[#fbfbfb5b] flex items-center m-auto w-2/3 rounded-lg text-sm">
           <Search className="absolute left-3 top-2.5 text-gray-500" />
           <input
             type="text"
             placeholder="Find something"
-            className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-purple-400"
+            className="w-full pl-10 pr-4 py-3 bg-transparent focus:outline-none rounded-lg focus:ring-2 focus:ring-yellow-200"
           />
-          <button className="flex items-center px-3 py-1.5 bg-gray-200 rounded-full text-sm font-medium text-gray-700 shadow hover:bg-gray-300">
+          <button className="absolute right-2 flex items-center px-3 py-1.5 bg-white rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-200">
             ⌘ K
           </button>
         </div>
