707f64a1c038f90d8c227f7cac0922f3d4b62240
SylvHardDev <hardsyldev@gmail.com>
2024-12-13 09:08:48 +0300

    v1 dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,27 @@
-import "./Dashboard.css"
+import "./Dashboard.css";
+import Sidebar from "../components/Sidebar/Sidebar";
+import Navbar from "../components/Navbar/Navbar";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="flex h-screen bg-gray-50">
+      {/* Sidebar */}
+      <Sidebar />
 
-export default Dashboard
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Placeholder for other content */}
+        <div className="p-6">
+          <h2 className="text-2xl font-bold text-gray-800">
+            Dashboard Content
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
