d8d35232f27fc1d9617af517ed204dbda1245c91
SylvHardDev <hardsyldev@gmail.com>
2024-12-13 11:54:04 +0300

    add icon close sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -11,58 +11,63 @@ import {
   User,
   Settings,
   HelpCircle,
+  PanelLeftClose,
 } from "lucide-react";
 
 const Sidebar = () => {
   return (
     <div className="sidebar bg-gradient-to-b from-gray-50 via-white to-gray-100 w-64 h-screen flex flex-col justify-between p-4 border-r">
       {/* Header */}
-      <div className="flex items-center mb-6">
+      <div className="flex items-center mb-6 relative">
         <div className="flex items-center justify-center border border-lime-950 h-10 w-9">
           <span className="text-green-950 font-bold text-xl">N</span>
         </div>
         <h1 className="text-xl font-bold text-gray-800 ml-3">Nuklear</h1>
+        <div className="absolute right-0 hover:cursor-pointer">
+          <PanelLeftClose className="w-5 text-gray-600 mr-3" />
+        </div>
       </div>
-
       {/* Main Navigation */}
       <div>
-        <div className="mb-6">
+        <div className="mb-6 bg-[#F8F9FB] p-2 rounded-lg shadow-lg">
           <div className="flex items-center justify-between">
             <div className="flex items-center">
-              <CheckCircle className="text-purple-600 mr-3" />
+              <CheckCircle className="text-purple-600 w-5 mr-3" />
               <span className="text-gray-800 font-medium">Tasks</span>
             </div>
-            <span className="bg-purple-600 text-white text-sm px-2 py-0.5 rounded-full">
+            <span className="bg-white text-purple-600 text-sm px-1 py-0.5 rounded-lg">
               16
             </span>
           </div>
         </div>
 
-        <div className="flex items-center mb-6">
-          <Zap className="text-gray-500 mr-3" />
+        <div className="flex items-center mb-6 ml-2">
+          <Zap className="text-gray-500 mr-3 w-5" />
           <span className="text-gray-600 font-medium">Activities</span>
         </div>
+
+        <hr className="py-4" />
 
         <p className="text-gray-500 uppercase text-xs mb-3">Main</p>
         <nav className="mb-6">
           <div className="flex items-center p-2 rounded-lg hover:bg-gray-200 cursor-pointer">
-            <Grid className="text-gray-600 mr-3" />
+            <Grid className="w-5 text-gray-600 mr-3" />
             <span className="text-gray-700 font-medium">Dashboard</span>
           </div>
           <div className="flex items-center p-2 rounded-lg hover:bg-gray-200 cursor-pointer">
-            <Calendar className="text-gray-600 mr-3" />
+            <Calendar className="w-5 text-gray-600 mr-3" />
             <span className="text-gray-700 font-medium">Schedule</span>
           </div>
           <div className="flex items-center p-2 rounded-lg hover:bg-gray-200 cursor-pointer">
-            <FileText className="text-gray-600 mr-3" />
+            <FileText className="w-5 text-gray-600 mr-3" />
             <span className="text-gray-700 font-medium">Note</span>
           </div>
           <div className="flex items-center p-2 rounded-lg hover:bg-gray-200 cursor-pointer">
-            <Box className="text-gray-600 mr-3" />
+            <Box className="w-5 text-gray-600 mr-3" />
             <span className="text-gray-700 font-medium">Products</span>
           </div>
           <div className="flex items-center p-2 rounded-lg hover:bg-gray-200 cursor-pointer">
-            <BarChart className="text-gray-600 mr-3" />
+            <BarChart className="w-5 text-gray-600 mr-3" />
             <span className="text-gray-700 font-medium">Report</span>
           </div>
         </nav>
@@ -70,11 +75,11 @@ const Sidebar = () => {
         <p className="text-gray-500 uppercase text-xs mb-3">Records</p>
         <nav>
           <div className="flex items-center p-2 rounded-lg hover:bg-gray-200 cursor-pointer">
-            <Users className="text-gray-600 mr-3" />
+            <Users className="w-5 text-gray-600 mr-3" />
             <span className="text-gray-700 font-medium">Team</span>
           </div>
           <div className="flex items-center p-2 rounded-lg hover:bg-gray-200 cursor-pointer">
-            <User className="text-gray-600 mr-3" />
+            <User className="w-5 text-gray-600 mr-3" />
             <span className="text-gray-700 font-medium">Clients</span>
           </div>
         </nav>
@@ -83,11 +88,11 @@ const Sidebar = () => {
       {/* Footer */}
       <div>
         <div className="flex items-center p-2 rounded-lg hover:bg-gray-200 cursor-pointer mb-3">
-          <Settings className="text-gray-600 mr-3" />
+          <Settings className="w-5 text-gray-600 mr-3" />
           <span className="text-gray-700 font-medium">Settings</span>
         </div>
         <div className="flex items-center p-2 rounded-lg hover:bg-gray-200 cursor-pointer">
-          <HelpCircle className="text-gray-600 mr-3" />
+          <HelpCircle className="w-5 text-gray-600 mr-3" />
           <span className="text-gray-700 font-medium">Support</span>
         </div>
         <div className="flex items-center p-4 bg-gray-100 rounded-lg mt-6">
