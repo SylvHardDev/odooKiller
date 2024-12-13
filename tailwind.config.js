c630ae481370dcbec277e433c9de3944ff198218
SylvHardDev <hardsyldev@gmail.com>
2024-12-13 10:08:15 +0300

    add custome linear gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(270deg, rgba(255,255,255,1) 0%, rgba(249,211,211,1) 21%, rgba(234,251,252,1) 46%, rgba(255,255,255,1) 100%, rgba(99,229,255,1) 100%)',
+      },
+    },
   },
   plugins: [],
 };
